0536708c5622ae21da2fe6264cdeaf82edfa36ba
ranggad46 <rangga.drendra46@gmail.com>
2023-10-13 04:50:58 +0700

    add estimasi harga

--- js/produk.js
+++ js/produk.js
@@ -1,3 +1,56 @@
+// Estimasi Harga
+function getHarga(){
+  let from = document.getElementById("asal").value;
+  let to = document.getElementById("tujuan").value;
+  let weigth = document.getElementById("berat").value;
+  let length = document.getElementById("panjang").value;
+  let heigth = document.getElementById("tinggi").value;
+  let width = document.getElementById("lebar").value;
+  
+  
+  let tableBody = document.querySelector('#data-estimasi tbody');
+
+// Mendefinisikan URL API
+let apiUrl = `https://magnificent-cod-slip.cyclic.app/products/${from}/${to}`;
+
+  
+
+// Mengambil data dari API
+fetch(apiUrl)
+.then(response => response.json())
+.then(data => {
+  // Loop melalui data dan tambahkan baris ke dalam tabel
+  data.forEach(item => {
+    let biaya_normal =parseInt(`${item.harga_normal}`)
+    let tambahan = parseInt(`${item.tambahan}`);
+    let volume = parseInt(length * heigth * width / 10000) // Mengonversi cm³ ke liter.
+    let harga = 0
+        
+    if (volume > weigth){
+      harga = biaya_normal + (volume - 10) * tambahan 
+    }else if (weigth > 10.3){
+      harga = (weigth - 10) * tambahan + biaya_normal
+    }else{
+      harga = biaya_normal
+    }
+            const row = document.createElement('tr');
+
+            row.innerHTML = `
+            <td>${item.nama_paket}</td>
+                <td>${harga}</td>
+                `;
+                tableBody.appendChild(row);
+              });
+    })
+    .catch(error => {
+      console.error('Terjadi kesalahan:', error);
+    });
+    
+  }
+
+
+// Get Outlate
+
 // Mendapatkan referensi ke elemen tabel
 const tableBody = document.querySelector('#data-table tbody');
 
